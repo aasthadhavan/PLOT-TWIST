802815f70801078e2c5163b0546e8d5325f8eb7e
aasthadhavan <aasthadhavan@gmail.com>
2026-03-29 15:48:55 +0530

    Lead: Implementing GitHub Actions for Continuous Deployment

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Please log in to continue.'
+
 login_manager.login_message_category = 'info'
 
 @login_manager.user_loader
